7122c12c873bbde0cd31b05f7a1b89c2f70e75c3
Глеб Безруков <glebon84@gmail.com>
2024-03-11 12:47:36 +0600

    Добавил миграции

--- WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e05462ffb8707bf7da42f3a76a795f3275734c5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1a1a09126220e736fee33d91e7e0954a327e97d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
